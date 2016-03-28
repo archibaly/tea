0698c3d8651a2e14f58a882419d76a21e81ba0f7
qianqiangliu <qianqiangliu@hotmail.com>
2016-03-28 10:13:16 +0800

    change key to string

--- main.c
+++ main.c
@@ -3,19 +3,20 @@
 
 #include "tea.h"
 
+#define KEY	"4nu&f%0(*&<%!@*7"
+
 int main()
 {
-	uint8_t buf[128] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-	uint8_t key[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+	uint8_t buf[32] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-	int n = tea_encrypt((uint8_t *)buf, 23, key);
+	int n = tea_encrypt((uint8_t *)buf, 20, KEY);
 
 	int i;
 	for (i = 0; i < n; i++)
-		printf("%d", buf[i]);
+		printf("%d ", buf[i]);
 	printf("\n");
 
-	n = tea_decrypt((uint8_t *)buf, n, key);
+	n = tea_decrypt((uint8_t *)buf, n, KEY);
 
 	for (i = 0; i < n; i++)
 		printf("%d", buf[i]);
